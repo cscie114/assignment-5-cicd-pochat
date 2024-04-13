7c920b21b01df857a2ad492d31cd1afc12e6f5df
pochat <mariopochat@gmail.com>
2024-04-12 20:27:27 -0700

    Updated netlify.yml

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,9 @@ export default function index() {
 
     <div className='h-screen flex flex-col text-2xl items-center justify-center'>
       <p>CI/CD</p>
-      <p className='text-blue-500'>Mario test Lint</p>
+      <p className='text-red-500'>Mario test Lint</p>
       <p className='text-blue-500'>Added Jest</p>
+      <p className='text-green-500'>To netlify.yml</p>
     </div>
    
     
