8bf8fd0f6bad7f57824cf93156d49b9bdca19b17
pochat <mariopochat@gmail.com>
2024-04-14 17:33:51 -0700

    added youtube api fetch

---
+++ gatsby-node.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+console.log("process.env.GATSBY_YOUTUBE_API");
+console.log(process.env.GATSBY_YOUTUBE_API);
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  // get data from YouTube API at build time
+  const result = await fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUQeRaTukNYft1_6AZPACnog&key=${process.env.GATSBY_YOUTUBE_API}`)
+
+  console.log("result");
+  console.log(result);
+
+  const resultData = await result.json();
+
+  console.log("Result Data:");
+  console.log(resultData); // Log the resultData to see its structure
+
+  // If resultData.items is undefined, exit early to avoid the error
+  if (!resultData.items) {
+    console.error("No items found in result data");
+    return;
+  }
+
+  // create nodes for each item in the result
+  resultData.items.forEach(item => {
+    createNode({
+      // add necessary fields from the item
+      // For example, if you want to include video title and description:
+      title: item.snippet.title,
+      description: item.snippet.description,
+      id: item.id,
+      parent: null,
+      children: [],
+      internal: {
+        type: `YoutubeVideo`,
+        contentDigest: createContentDigest(item),
+      },
+    });
+  });
+};
