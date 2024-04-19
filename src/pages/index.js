336a3879177d7bdf794a4cea7deb3e2dd80ec11f
pochat <mariopochat@gmail.com>
2024-04-18 17:34:38 -0700

    testing serverless

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,52 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React, { useState } from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+
 
 export default function IndexPage({ data }) {
+  const [message, setMessage] = useState('');
+
+  const helloClick = async () => {
+    try {
+      const response = await fetch('/netlify/functions/hello-world'); 
+      console.log("response");
+      console.log(response);
+      const data = await response.json();
+      console.log("data");
+      console.log(data);
+      setMessage(data.message);
+    } catch (error) {
+      console.error('Error fetching message:', error);
+    }
+  };
+  
+
   return (
     <Layout>
-    <div className=''>
-      <h1 className="text-center text-3xl font-bold mb-8 mt-8">Fetching videos with the Youtube API:</h1>
+      <div className=''>
+        <h1 className="text-center text-3xl font-bold mb-8 mt-8">Fetching 6 videos with the Youtube API:</h1>
+        <button onClick={helloClick} className='flex m-auto p-5 bg-slate-200'>Click to run the serverless function</button>
+        {message && <p>{message}</p>}
 
-      {/* Render the fetched video titles */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data.allYoutubeVideo.nodes.map(node => (
-          <div key={node.title} className="bg-white shadow-md rounded-md p-4">
-            <h2 className="text-xl font-semibold mb-2">{node.title}</h2>
-            <iframe
-              width="100%"
-              height="315"
-              src={`https://www.youtube.com/embed/${node.videoId}`}
-              title={node.title}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        ))}
+        {/* Render the fetched video titles */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {data.allYoutubeVideo.nodes.map(node => (
+            <div key={node.title} className="bg-white shadow-md rounded-md p-4">
+              <h2 className="text-xl font-semibold mb-2">{node.title}</h2>
+              <iframe
+                width="100%"
+                height="315"
+                src={`https://www.youtube.com/embed/${node.videoId}`}
+                title={node.title}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </Layout>
-  )
+  );
 }
 
 // Query to fetch YouTube video titles
@@ -40,4 +60,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
